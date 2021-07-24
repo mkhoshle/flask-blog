db464ae80102a48f3e677374e6bf23192b0bf08d
Cloud User <centos@ip-172-31-19-5.us-east-2.compute.internal>
2021-07-24 13:36:45 +0000

    Added back up step

---
+++ prod-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# test Flask endpoints
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate mlh
+
+#source "/home/centos/anaconda3/envs/mlh/bin/activate"
+endpoints=$(python -c "from app import app; a=[rule.endpoint for rule in app.url_map.iter_rules()]; print(*a)")
+
+echo $endpoints
+
+for rule in $endpoints
+    do
+	echo $rule
+	curl -LI https://mahzadkhoshlessan.duckdns.org/${rule} -o /dev/null -w '%{http_code}\n' -s
+    done
+
+
+if [[ $? -ne 0 ]] ; then
+    echo "Tests failed"
+    exit 1
+else
+    echo "All tests passed successfully"
+fi
